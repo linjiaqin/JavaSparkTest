6d4b7109404dfb927420daa55ce0b30ac5ff51cf
linjiaqin <1263457581@qq.com>
2019-03-02 17:56:40 +0800

    the pom and wordcount change

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,7 +1,9 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -20,20 +22,27 @@ public class WordCount {
         String input = hdfs + "/linjiaqin/a.txt";
 
         JavaRDD<String> text = sc.textFile(input);
-        //JavaRDD<String> words =  text.filter(line -> line.contains("python"));
-        JavaRDD<String> words = text.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 
-        JavaRDD<String> flatMapRDD = text.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterator<String> call(String s) throws Exception {
-                String[] split = s.split("\\s+");
-                return Arrays.asList(split).iterator();
-            }
-        });
-        List<String> wordsList = words.collect();
-        wordsList.forEach(x -> System.out.println(x));
-//        for(String x: wordsList)
-//            System.out.println(x);
+        JavaRDD<String> words = text.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+        JavaPairRDD<String, Integer> counts = words.mapToPair(x -> new Tuple2<>(x, 1)).reduceByKey((a,b) -> a+b);
+        //List<String> wordsList = words.collect();
+        //wordsList.forEach(x -> System.out.println(x));
+        JavaPairRDD<String, Integer> sortCounts = counts.sortByKey();
+        List countlist = sortCounts.collect();
+        countlist.forEach(x -> System.out.println(x));
+
+        //JavaRDD<String> words =  text.filter(line -> line.contains("python"));
+//        JavaRDD<String> flatMapRDD = text.flatMap(new FlatMapFunction<String, String>() {
+//            @Override
+//            public Iterator<String> call(String s) throws Exception {
+//                String[] split = s.split("\\s+");
+//                return Arrays.asList(split).iterator();
+//            }
+//        });
+        //        for(String x: wordsList)
+        //        System.out.println(x);
+
+
 
     }
 }
