7c34e704b1789b0af516ae722b2d354419986b41
linjiaqin <1263457581@qq.com>
2019-03-02 17:52:55 +0800

    Squar test

---
+++ src/main/java/Squar.java
@@ -0,0 +1,16 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
+public class Squar {
+    public static void main(String[] args){
+        SparkConf conf = new SparkConf().setAppName("squar").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3,4,5));
+        JavaRDD<Integer> res = rdd.map(x -> x*x);
+        System.out.println(res.collect());
+    }
+}
