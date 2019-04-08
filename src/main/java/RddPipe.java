ba0c13824f8451b8b4ca747ca18bbf49a074fc28
linjiaqin <1263457581@qq.com>
2019-04-08 20:07:47 +0800

    pipe 连接集群

--- src/main/java/RddPipe.java
+++ src/main/java/RddPipe.java
@@ -8,18 +8,19 @@ import java.util.Arrays;
 
 public class RddPipe {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setMaster("local[4]").setAppName("PipeTest");
+        SparkConf conf = new SparkConf().setMaster("spark://192.168.0.100:7077").setAppName("PipeTest");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        //String[] jarPath = new String[]{"/home/linjiaqin/IdeaProjects/JavaSparkTest/out/artifacts/wordcount/wordcount.jar"};
-        //conf.setJars(jarPath);
 
-        String hdfs = "hdfs://localhost:9000";
-        String cPath = "file:/home/linjiaqin/sh/hello";
-
-        sc.addFile(cPath);
+        String cPath = "file:/home/linjiaqin/sh/sparktest/hello";
+        sc.addFile(cPath,true);    //很玄学，这里需要true，然后运行那里不是
+        String jarPath= "/home/linjiaqin/IdeaProjects/JavaSparkTest/out/artifacts/RddPipe/RddPipe.jar";
+        sc.addJar(jarPath);
         JavaRDD<Integer> a = sc.parallelize(Arrays.asList(1,2,3,4,5,6,7,9));   //这里rdd作为参数输入
-        JavaRDD<String> rdd = a.pipe(SparkFiles.get("hello"));       //可以看出开的进程数和核数相关,与rdd的个数无关，而且还和cin的接收有关
+        a.collect().forEach(x-> System.out.println(SparkFiles.get("hello")));
+        //a.foreach(x-> System.out.println(SparkFiles.get("hello")));
+        JavaRDD<String> rdd = a.pipe("./hello");     //SparkFiles.get("hello")local模式的时候用这个就行
+        //可以看出开的进程数和核数相关,与rdd的个数无关，而且还和cin的接收有关
         //如果每个c程序只接受一个参数，只会执行核数次而不是将所有的rdd都执行完
         rdd.collect().forEach(x-> System.out.println("haha"+x));
         JavaRDD<Integer> b = a.map(x->x+1);
