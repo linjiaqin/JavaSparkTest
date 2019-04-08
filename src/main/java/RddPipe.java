264281004a441e5ea0f23101afde1677cf236bd0
linjiaqin <1263457581@qq.com>
2019-04-08 20:47:15 +0800

    pipe 连接集群

--- src/main/java/RddPipe.java
+++ src/main/java/RddPipe.java
@@ -12,14 +12,14 @@ public class RddPipe {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
 
-        String cPath = "file:/home/linjiaqin/sh/sparktest/hello";
+        String cPath = "file:/home/linjiaqin/sh/sparktest/hello1";
         sc.addFile(cPath,true);    //很玄学，这里需要true，然后运行那里不是
         String jarPath= "/home/linjiaqin/IdeaProjects/JavaSparkTest/out/artifacts/RddPipe/RddPipe.jar";
         sc.addJar(jarPath);
-        JavaRDD<Integer> a = sc.parallelize(Arrays.asList(1,2,3,4,5,6,7,9));   //这里rdd作为参数输入
-        a.collect().forEach(x-> System.out.println(SparkFiles.get("hello")));
+        JavaRDD<Integer> a = sc.parallelize(Arrays.asList(1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20)).repartition(4);   //这里rdd作为参数输入
+        a.collect().forEach(x-> System.out.println(SparkFiles.get("hello1")));
         //a.foreach(x-> System.out.println(SparkFiles.get("hello")));
-        JavaRDD<String> rdd = a.pipe("./hello");     //SparkFiles.get("hello")local模式的时候用这个就行
+        JavaRDD<String> rdd = a.pipe("./hello1");     //SparkFiles.get("hello")local模式的时候用这个就行
         //可以看出开的进程数和核数相关,与rdd的个数无关，而且还和cin的接收有关
         //如果每个c程序只接受一个参数，只会执行核数次而不是将所有的rdd都执行完
         rdd.collect().forEach(x-> System.out.println("haha"+x));
